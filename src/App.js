33a3398eec8bad35a52ec029de9db6f673bbe76f
Sergio Pineda <sergio.pinedas94@gmail.com>
2021-06-02 07:46:34 -0500

    Se crea la funcionalidad de eliminar y editar objetos con los metodos PUT y DELETE

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 const HOST_API = "http://localhost:8080/api"
 
 const initialState = {
-  list: []
+  list: [],
+  item: {}
 };
 
 const Store = createContext(initialState)
 
 const Form = () => {
   const formRef = useRef(null);
-  const { dispatch } = useContext(Store);
-  const [state, setState] = useState({});
+  const { dispatch, state: { item } } = useContext(Store);
+  const [state, setState] = useState(item);
 
 
   const onAdd = (event) => {
@@ -19,13 +20,12 @@ const Form = () => {
 
     const request = {
       name: state.name,
-      description: state.description,
       id: null,
       isComplete: false
     };
 
 
-    fetch(HOST_API+"/todo", {
+    fetch(HOST_API + "/todo", {
       method: "POST",
       body: JSON.stringify(request),
       headers: {
@@ -40,11 +40,38 @@ const Form = () => {
       })
   }
 
+  const onEdit = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      description: state.description,
+      id: item.id,
+      isComplete: item.isCompleted
+    };
+
+
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      })
+  }
+
   return <form ref={formRef}>
-    <input type='text' name='name' onChange={(event) => {
+    <input type='text' name='name' defaultValue={item.name} onChange={(event) => {
       setState({ ...state, name: event.target.value })
     }}></input>
-    <button onClick={onAdd}>Agregar</button>
+    {item.id && <button onClick={onEdit}>Actualizar</button>}
+    {!item.id && <button onClick={onAdd}>Agregar</button>}
   </form>
 }
 
@@ -58,6 +85,21 @@ const List = () => {
         dispatch({ type: 'update-list', list })
       })
   }, [state.list.length, dispatch]);
+
+  const onDelete = (id) => {
+    fetch(HOST_API + "/"+id+"/todo", {
+      method: "DELETE"
+    })
+    .then((list) => {
+      dispatch({ type: "delete-item", id })
+    })
+  };
+
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo })
+  }
+
   return <div>
     <table>
       <thead>
@@ -72,8 +114,9 @@ const List = () => {
           return <tr key={todo.id}>
             <td>{todo.id}</td>
             <td>{todo.name}</td>
-            <td>{todo.description}</td>
-            <td>{todo.isCompleted}</td>
+            <td>{todo.isCompleted === true? "SI": "NO"}</td>
+            <td><button onClick={() => onDelete(todo.id)} >Eliminar</button></td>
+            <td><button onClick={() => onEdit(todo)} >Editar</button></td>
           </tr>
         })}
       </tbody>
@@ -83,12 +126,32 @@ const List = () => {
 
 function reducer(state, action) {
   switch (action.type) {
+    case 'delete-item':
+      const listUpdate = state.list.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, list: listUpdate }
+
     case 'update-list':
       return { ...state, list: action.list }
+
+    case 'update-item': 
+      const listUpdateEdit = state.list.map((item) => {
+        if (item.id === action.item.id) {
+          return action.item
+        }
+        return item;
+      });
+      return { ...state, list: listUpdateEdit, item: {} }
+
     case 'add-item':
       const newList = state.list;
       newList.push(action.item);
       return { ...state, list: newList }
+
+    case 'edit-item':
+      return { ...state, item: action.item }
+
     default:
       return state;
   }
